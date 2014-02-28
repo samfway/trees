a3443381d90e80a14e4009ab2dcdec52eee92774
samfway <samfway@gmail.com>
2014-02-28 12:41:52 -0700

    77.7%

--- tree_lib/predict.py
+++ tree_lib/predict.py
@@ -11,6 +11,8 @@ __status__ = "Development"
 from .parse import parse_csv_columns
 from sklearn import grid_search, preprocessing
 from sklearn.svm import SVC
+from sklearn.pipeline import Pipeline
+from sklearn.decomposition import TruncatedSVD
 import pickle
 
 def load_pickle(model_pickle_file):
@@ -21,15 +23,26 @@ def build_models(training_file, output_file=None, scale_file=None):
     """ Build the models desired for testing """ 
     models = [] 
     labels, matrix = parse_csv_columns(training_file, labeled=True)
-   
+  
+    priors = [0.365, 0.488, 0.062, 0.005, 0.016, 0.030, 0.035]
+    class_w = { i+1:priors[i] for i in xrange(len(priors)) }
+ 
     # Preprocess data first!
     scaler = preprocessing.StandardScaler()
     matrix = scaler.fit_transform(matrix)
+    #redux = TruncatedSVD(25) 
+    #matrix = redux.fit_transform(matrix)    
+    data_prep = Pipeline([('scaling', scaler)]) #, ('ksvd', redux)])
  
     # 1 - SVM 
-    #svm_params = {'kernel':['rbf'], 'C':[8, 9, 10, 11, 12]}
+    #svm_params = {'kernel':['rbf', 'poly', 'sigmoid'], 'C':[8, 10, 12]}
+    #svm_params = [
+    #    {'C': [1, 10, 100, 1000], 'kernel': ['poly']},
+    #    {'C': [1, 10, 100, 1000], 'gamma': [0.001, 0.0001], 'kernel': ['rbf']},
+    #]
     #clf = grid_search.GridSearchCV(SVC(), svm_params, cv=5)
-    clf = SVC(kernel='rbf', C=10)
+    clf = SVC(kernel='rbf', C=12) 
+    #clf = SVC(kernel='poly', C=100, class_weight='auto')
     clf.fit(matrix, labels)
     models.append( ('svm',clf) )
     
@@ -37,7 +50,7 @@ def build_models(training_file, output_file=None, scale_file=None):
         pickle.dump(models, open(output_file, "wb"))
 
     if scale_file is not None:
-        pickle.dump(scaler, open(scale_file, "wb"))
+        pickle.dump(data_prep, open(scale_file, "wb"))
     
-    return models, scaler
+    return models, data_prep 
 
