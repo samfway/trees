4086aacdff05c4da12782468e6542d053e18b609
samfway <samfway@gmail.com>
2014-02-27 23:46:34 -0700

    Makin models outta stuff

--- build_models.py
+++ build_models.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 import argparse
 from tree_lib.parse import parse_csv_columns
+from tree_lib.predict import build_models, load_models
 
 def interface():
     args = argparse.ArgumentParser()
     args.add_argument('-i', '--input-file', help='Input data matrix', required=True)
+    args.add_argument('-o', '--output-file', help='Models file (.pkl)', default='models.pkl')
     args = args.parse_args()
     return args
 
 if __name__=="__main__":
     args = interface()
-    labels, matrix = parse_csv_columns(args.input_file, labeled=True)
-    print len(labels)
-    print matrix.shape
+    build_models(args.input_file, args.output_file)
 
