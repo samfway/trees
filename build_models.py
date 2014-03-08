c4b720f7cdf8103b6dbf4ae37a4863a1d1746395
Sam Way <samfway@gmail.com>
2014-03-08 11:40:26 -0700

    inconsistent parameter letters

--- build_models.py
+++ build_models.py
@@ -6,12 +6,12 @@ from tree_lib.predict import build_models, load_pickle
 def interface():
     args = argparse.ArgumentParser()
     args.add_argument('-i', '--input-file', help='Input data matrix', required=True)
-    args.add_argument('-o', '--output-file', help='Models file (.pkl)', default='models.pkl')
+    args.add_argument('-m', '--model-file', help='Models file (.pkl)', default='models.pkl')
     args.add_argument('-s', '--scale-file', help='Scaling file (.pkl)', default='scale.pkl')
     args = args.parse_args()
     return args
 
 if __name__=="__main__":
     args = interface()
-    build_models(args.input_file, args.output_file, args.scale_file)
+    build_models(args.input_file, args.model_file, args.scale_file)
     
