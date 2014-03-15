1e0a7cdaf4b5f8b582c6755ac92f073448c66016
Sam Way <samfway@gmail.com>
2014-03-15 11:37:24 -0600

    Cleaned up eval

--- evaluate_models.py
+++ evaluate_models.py
@@ -2,6 +2,10 @@
 import argparse
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
+from sklearn.dummy import DummyClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.naive_bayes import MultinomialNB, GaussianNB
 from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
 from tree_lib.parse import parse_csv_columns
 from ml_utils.util import convert_labels_to_int
@@ -27,9 +31,15 @@ if __name__=="__main__":
 
     # Load up all desired models
     models =  [] 
-    models.append(('Extra Trees', ExtraTreesClassifier(n_estimators=10, \
-        criterion='entropy', max_features=10, bootstrap=False)))
+    models.append(('Most Freq', DummyClassifier(strategy='most_frequent')))
+    models.append(('Stratified', DummyClassifier(strategy='stratified')))
+    models.append(('Uniform', DummyClassifier(strategy='uniform')))
+    models.append(('Log. Regr', LogisticRegression()))
+    models.append(('Naive Bayes', GaussianNB()))
+    models.append(('SVM (RBF)', SVC(kernel='rbf', C=50, gamma=0.01)))
     models.append(('Random Forest', RandomForestClassifier(n_estimators=10, \
+        criterion='entropy', max_features=10, bootstrap=False)))
+    models.append(('Extra Trees', ExtraTreesClassifier(n_estimators=10, \
         criterion='entropy', max_features=10, bootstrap=False)))
 
     # Load up all desired performance metrics
